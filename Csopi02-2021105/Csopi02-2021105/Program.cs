7e85b49094c0951d16c9be92a3ebc7a43e20041b
bszebi <120450969+bszebi@users.noreply.github.com>
2024-11-05 14:14:54 +0100

    Add files via upload

---
+++ Csopi02-2021105/Csopi02-2021105/Program.cs
@@ -0,0 +1,55 @@
+﻿using Csopi02_2021105;
+using System.Text;
+
+const string FILE = @"..\..\..\src\forras.txt";
+const int AKTUALIS_EV = 2014;
+
+List<versenyzo> versenyzok = new();
+
+using StreamReader sr = new(FILE, Encoding.UTF8);
+while (!sr.EndOfStream) versenyzok.Add(new versenyzo(sr.ReadLine()));
+
+Console.WriteLine($"Versenyt befejezok szama: {versenyzok.Count} fo");
+
+// LINQ lekérdezések
+
+var f01 = versenyzok.Count(v => v.Kategoria == "elit junior");
+
+var f02 = versenyzok
+    .Where(v => v.Nem)
+    .Average(v => AKTUALIS_EV - v.SzulEv);
+
+var f03 = versenyzok
+    .Sum(v => v.VersenyIdok["Futás"].TotalHours);
+
+var f04 = versenyzok
+    .Where(v => v.Kategoria == "20-24")
+    .Average(v => v.VersenyIdok["Úszás"].TotalMinutes);
+
+var f05 = versenyzok
+    .Where(v => !v.Nem)
+    .MinBy(v => v.OsszIdo);
+
+var f06 = versenyzok
+    .GroupBy(v => v.Nem)
+    .ToDictionary(g => g.Key, g => g.Count());
+
+var f07 = versenyzok
+    .GroupBy(v => v.Kategoria)
+    .ToDictionary(g => g.Key, g => g.Average(
+        v => v.VersenyIdok["I. depó"].TotalMinutes + v.VersenyIdok["II. depó"].TotalMinutes));
+
+// Kiírások
+
+Console.WriteLine($"Versenyzők száma 'elit junior' kategóriában: {f01} fő");
+Console.WriteLine($"Férfi versenyzők átlagéletkora: {f02:0.00} év");
+Console.WriteLine($"A versenyzők futással töltött összideje: {f03:0.00} óra");
+Console.WriteLine($"Átlagos úszási idő 20-24 kategóriában: {f04:0.00} perc");
+Console.WriteLine($"Női győztes: {f05}");
+Console.WriteLine("Nemek szerint a versenyt befejezők száma:");
+foreach (var kvp in f06)
+    Console.WriteLine($"\t{(kvp.Key ? "Férfi" : "Nő")}: {kvp.Value} fő");
+
+Console.WriteLine("Korkategóriánként az átlag depóban töltött idő:");
+foreach (var kvp in f07)
+    Console.WriteLine($"\t{kvp.Key}: {kvp.Value:0.00} perc");
---
+++ Csopi02-2021105/Csopi02-2021105/versenyzo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csopi02_2021105
+{
+    internal class versenyzo
+    {
+        public string Nev { get; set; }
+        public int SzulEv { get; set; }
+        public string RajtSzam { get; set; }
+        public bool Nem { get; set; } // true = férfi, false = nő
+        public string Kategoria { get; set; }
+        public Dictionary<string, TimeSpan> VersenyIdok { get; set; }
+
+        public int OsszIdo => (int)VersenyIdok.Values.Sum(x => x.TotalSeconds);
+
+        public override string ToString() =>
+            $"[{RajtSzam}] {Nev} ({Kategoria}, {(Nem ? "férfi" : "nő")})";
+
+        public versenyzo(string sor)
+        {
+            var v = sor.Split(';');
+            Nev = v[0];
+            SzulEv = int.Parse(v[1]);
+            RajtSzam = v[2];
+            Nem = v[3] == "f";
+            Kategoria = v[4];
+            VersenyIdok = new()
+            {
+                { "Úszás",    TimeSpan.Parse(v[5]) },
+                { "I. depó",  TimeSpan.Parse(v[6]) },
+                { "Kerékpár", TimeSpan.Parse(v[7]) },
+                { "II. depó", TimeSpan.Parse(v[8]) },
+                { "Futás",    TimeSpan.Parse(v[9]) },
+            };
+        }
+    }
+}
